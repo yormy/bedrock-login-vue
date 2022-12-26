c6f2a650267c2dc4215353fd8e4a422178798182
rob <dev@mexion.com>
2022-12-26 08:43:40 +0100

    add login exports

--- src/Auth/exports.js
+++ src/Auth/exports.js
@@ -1,7 +1,22 @@
 import YLoginWrapper from "./LoginWrapper.vue"
 import YLoginHeader from "./LoginHeader.vue"
+import YResetLoginname from "./Components/ResetLoginname.vue"
+import YResetPassword from "./Components/ResetPassword.vue"
+
+import YLoginEmailCode from "./Components/LoginEmailCode.vue"
+import YLoginCode from "./Components/LoginCode.vue"
+import YLoginWait from "./Components/LoginWait.vue"
+import YResetSelector from "./Components/ResetSelector.vue"
 
 export {
     YLoginWrapper,
     YLoginHeader,
+    YResetLoginname,
+    YResetPassword,
+    YLoginEmailCode,
+    YLoginCode,
+    YLoginWait,
+    YResetSelector
+
+
 }
